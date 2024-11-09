78c7175b95fd3415ee7da3a930de39e65264d8e0
“Gui” <ggsantana18@gmail.com>
2024-11-09 11:29:50 -0300

    att14

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-// Seleciona todas asimagens do slider
-const imagens = document.querySelectorAll("#slider img");
+// // Seleciona todas asimagens do slider
+// const imagens = document.querySelectorAll("#slider img");
 
-// Variável para controlar qual imagem está sendo exibida no momento
-let imagemAtual = 0;
+// // Variável para controlar qual imagem está sendo exibida no momento
+// let imagemAtual = 0;
 
-// Função para mostrar uma imagem específica
-function mostrarImagem(index) {
-  // Primeiro, escondemos todas as imagens
-  imagens.forEach((img) => (img.style.display = "none"));
+// // Função para mostrar uma imagem específica
+// function mostrarImagem(index) {
+//   // Primeiro, escondemos todas as imagens
+//   imagens.forEach((img) => (img.style.display = "none"));
+//   // Depois, exibimos a imagem com o índice indicado
+//   imagens[index].style.display = "block";
+// }
 
-  // Depois, exibimos a imagem com o índice indicado
-  imagens[index].style.display = "block";
-}
+// // Função para ir para a próxima imagem
+// function proximaImagem() {
+//   // Incremente o índice da imagem atual e volta para início se chegar ao final
+//   imagemAtual = (imagemAtual + 1) % imagens.length;
+//   mostrarImagem(imagemAtual);
+// }
 
-// Função para ir para a próxima imagem
-function proximaImagem() {
-  // Incremente o índice da imagem atual e volta para início se chegar ao final
-  imagemAtual = (imagemAtual + 1) % imagens.length;
-  mostrarImagem(imagemAtual);
-}
+// // Função para ir para a imagem anterior
+// function imagemAnterior() {
+//   // Decrementa o índice da imagem atual e volta para o final se for negativo
+//   imagemAtual = (imagemAtual - 1 + imagens.length) % imagens.length;
+//   mostrarImagem(imagemAtual);
+// }
 
-// Função para ir para a imagem anterior
-function imagemAnterior() {
-  // Decrementa o índice da imagem atual e volta para o final se for negativo
-  imagemAtual = (imagemAtual - 1 + imagens.length) % imagens.length;
-  mostrarImagem(imagemAtual);
-}
+// // Exibe a primeira imagem quando a página carrega
+// mostrarImagem(imagemAtual);
 
-// Exibe a primeira imagem quando a página carrega
-mostrarImagem(imagemAtual);
+// // Seleciona os botões de navegação
+// const botaoProximo = document.getElementById("nextBtn");
+// const botaoAnterior = document.getElementById("prevBtn");
 
-let intervalo = setInterval(proximaImagem, 2000)
+// // Adiciona o evento de clique ao botão "Próximo"
+// botaoProximo.addEventListener("click", () => {
+//   proximaImagem(); // Mostra a próxima imagem ao clicar
+// });
+
+// // Adicionar o evento de clique ao botão "Anterior"
+// botaoAnterior.addEventListener("click", () => {
+//   imagemAnterior(); // Mostra a imagem anterior ao clicar
+// });
 
 
+// _____________________ ANIMAÇÕES COM SCROLL
 
+const visu = new IntersectionObserver ((entries) =>{
+  entries.forEach( (entry) =>{
+    if (entry.isIntersecting) {
+      entry.target.classList.add("blocoText_show")
+    }
+    else{
+      entry.target.classList.remove("blocoText_show")
+    }
+  })
+})
 
+const elements = document.querySelectorAll(".blocoText")
+
+elements.forEach((element) => visu.observe(element))
 
